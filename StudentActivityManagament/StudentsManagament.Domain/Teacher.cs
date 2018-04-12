d054ab3b029e3a11c59d6e95b9284bcd46bc01d2
SFTLCRPC039\ciprian.sulu <cipriansulu1@yahoo.com>
2018-04-12 12:54:17 +0300

    added teacher to domain

---
+++ StudentActivityManagament/StudentsManagament.Domain/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsManagament.Domain
+{
+    class Teacher
+    {
+        public string Name { get; set; }
+        public List<Activity> Activities { get; set; }
+    }
+}

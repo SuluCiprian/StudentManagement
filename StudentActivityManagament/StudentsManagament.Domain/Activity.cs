eef39b4799ca9e30f0d6a6b08ed13a0771ede887
SFTLCRPC039\ciprian.sulu <cipriansulu1@yahoo.com>
2018-04-12 14:00:00 +0300

    added template activities button

--- StudentActivityManagament/StudentsManagament.Domain/Activity.cs
+++ StudentActivityManagament/StudentsManagament.Domain/Activity.cs
@@ -10,5 +10,6 @@ namespace StudentsManagament.Domain
         public string Name { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
+        public Teacher Owner { get; set; }
     }
 }

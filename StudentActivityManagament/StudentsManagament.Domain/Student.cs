cbd38cad608b28d21463b36ee7853ee9be066dbe
SFTLCRPC039\ciprian.sulu <cipriansulu1@yahoo.com>
2018-04-13 12:43:08 +0300

    Revert "refactor domain classes"
    This reverts commit f80fa4f4bd5e8554d8ea0aa213d81fc5faea2ffa.

--- StudentActivityManagament/StudentsManagament.Domain/Student.cs
+++ StudentActivityManagament/StudentsManagament.Domain/Student.cs
@@ -6,7 +6,6 @@ namespace StudentsManagament.Domain
 {
     public class Student
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public List<Activity> Activities { get; set; }
     }
--- StudentActivityManagament/StudentsManagament.Domain/Teacher.cs
+++ StudentActivityManagament/StudentsManagament.Domain/Teacher.cs
@@ -6,7 +6,6 @@ namespace StudentsManagament.Domain
 {
     public class Teacher
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public List<Activity> Activities { get; set; }
     }

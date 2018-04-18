150b4fb41b5e0e27b58012aec93e950e304b1220
SFTLCRPC039\ciprian.sulu <cipriansulu1@yahoo.com>
2018-04-18 12:39:46 +0300

    added activity views

--- StudentActivityManagament/StudentActivityManagement/Controllers/ActivitiesController.cs
+++ StudentActivityManagament/StudentActivityManagement/Controllers/ActivitiesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StudentActivityMenagement.Models.ActivityViewModel;
 using StudentsManagement.Persistence;
 
 namespace StudentActivityMenagement.Controllers
@@ -11,6 +12,7 @@ namespace StudentActivityMenagement.Controllers
     public class ActivitiesController : Controller
     {
         private readonly IUnitOfWork _unitOfwork;
+        private string user = null;
 
         public ActivitiesController(IUnitOfWork unitOfWork)
         {
@@ -37,6 +39,159 @@ namespace StudentActivityMenagement.Controllers
             var activities = _unitOfwork.Activities.GetAll();
 
             return View(activities);
+        }
+
+        public IActionResult ViewAll(string user)
+        {
+            if (user == "student")
+            {
+                List<string> activitiesName = new List<string>
+                {
+                    "Activity 1",
+                    "Activity 2",
+                    "Activity 3"
+                };
+
+                List<int> activitiesId = new List<int>
+                {
+                    1,
+                    2,
+                    3
+                };
+
+                List<string> activitiesType = new List<string>
+                {
+                    "Curs",
+                    "Seminar",
+                    "Llaborator"
+                };
+
+                List<string> activityDescription = new List<string>
+                {
+                    "Class 101",
+                    "Class 102",
+                    "Class 103"
+                };
+
+                var model = new Activities
+                {
+                    ActivitiesName = activitiesName,
+                    IdActivities = activitiesId,
+                    ActivitiesType = activitiesType,
+                    ActivitiesDescription = activityDescription
+                };
+
+                return View("StudentActivities", model);
+            }
+            else
+            {
+                List<string> activitiesName = new List<string>
+                {
+                    "Activity 4",
+                    "Activity 5",
+                    "Activity 6"
+                };
+
+                List<int> activitiesId = new List<int>
+                {
+                    4,
+                    5,
+                    6
+                };
+
+                List<string> activitiesType = new List<string>
+                {
+                    "S",
+                    "L",
+                    "C"
+                };
+
+                List<string> activityDescription = new List<string>
+                {
+                    "Class 104",
+                    "Class 105",
+                    "Class 106"
+                };
+
+                var model = new Activities
+                {
+                    ActivitiesName = activitiesName,
+                    IdActivities = activitiesId,
+                    ActivitiesType = activitiesType,
+                    ActivitiesDescription = activityDescription
+                };
+
+                return View("TeacherActivities", model);
+            }
+        }
+
+        public IActionResult Activity(int? id)
+        {
+            int idActivity = id ?? default(int);
+
+            if (user == "student")
+            {
+                List<DateTime> dateTime = new List<DateTime>
+                {
+                    new DateTime(2018, 5, 4),
+                    new DateTime(2018, 5, 5),
+                    new DateTime(2018, 5, 6),
+                    new DateTime(2018, 5, 7)
+                };
+
+                List<double> grade = new List<double>
+                {
+                    9,
+                    7.6,
+                    5,
+                    8
+                };
+
+                List<int> attendance = new List<int>
+                {
+                    5,
+                    2,
+                    0,
+                    6
+                };
+
+                var model = new StudentActivityInfo
+                {
+                    IdActivity = idActivity,
+                    Date = dateTime,
+                    Grade = grade,
+                    Attendance = attendance
+                };
+
+                return View("StudentActivity", model);
+            }
+            else
+            {
+                List<int> studentId = new List<int>
+                {
+                    1,
+                    2,
+                    3
+                };
+
+                List<string> name = new List<string>
+                {
+                    "Ionel",
+                    "Georgel",
+                    "Mihai"
+                };
+
+                string activityName = "E-learning";
+
+                var model = new AllStudentsOnActivity
+                {
+                    Id = studentId,
+                    Name = name,
+                    ActivityName = activityName
+                };
+
+                return View("TeacherActivity", model);
+            }
         }
     }
 }
